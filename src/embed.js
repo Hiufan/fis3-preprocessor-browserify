d01be96038673470c41e3099b13c305c7f6adcfe
MCF <mcf@MCF.local>
2016-12-21 15:32:23 +0800

    use the fis3 embed synax to process the __inline/__uri function

--- src/embed.js
+++ src/embed.js
@@ -1,38 +1,27 @@
 'use strict';
+var Transform = require('stream').Transform;
 
-var path = require('path');
-var through = require('through2');
-
-var URI_REG = /\b(__uri)\(\s*('|")([^'"]+)\2\s*\)/g; 
-var INLINE_REG = /\b(__inline)\(\s*('|")([^'"]+)\2\s*\)/g;
-
-/**
- * Browserify transform
- * change `__uri('path')` and `__inline('path')` to relative path from sourceFilePath
- */
+// 支持fis3内置语法
 module.exports = function (sourceFilePath) {
-    return function (inputFileRealPath) {
-        var data = [];
-
-        var transformFn = function (chunk, enc, cb) {
-            data.push(chunk);
+    return function(inputFileRealPath) {
+        var _transform = function(chunk, encoding, cb){
             cb && cb();
         };
 
-        var flushFn = function (cb) {
-            var replacer = function (match, embedFn, quote, depFileName) {
-                var depFileRealPath = path.resolve(path.dirname(inputFileRealPath), depFileName);
-                var depFileRelativePath = path.relative(path.dirname(sourceFilePath), depFileRealPath).replace(/\\/g, '/');
-                return embedFn + '(' + quote + depFileRelativePath + quote + ')';                
-            };
-
-            var contents = Buffer.concat(data).toString('utf8').replace(INLINE_REG, replacer).replace(URI_REG, replacer);
-
+        var _flush = function(cb){
+            var depFile = fis.file.wrap(inputFileRealPath);
+            fis.compile(depFile); 
+            var contents = depFile.getContent();
             this.push(contents);
 
             cb();
         };
 
-        return through(transformFn, flushFn);
-    };
+        var tr = new Transform({
+            transform: _transform,
+            flush: _flush
+        });
+
+        return tr;
+    }
 };
